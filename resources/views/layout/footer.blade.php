953bae79616fd91752984b03e912f848ca6b3dd1
frnxstd <burak@myself.com>
2018-07-31 16:31:13 +0300

    footer update

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,29 +1,20 @@
 <!-- Footer -->
-<footer class="footer">
+<footer class="footer m-6">
     <div class="container">
-        <div class="row gap-y align-items-center">
+        <div class="row">
+            <div class="col-md-8 col-lg-6 mx-auto">
 
-            <div class="col-6 col-lg-3">
-                <a href="{{ url('/') }}"><img src="{{ asset('assets/img/fornax-studio-logo.png') }}" alt="{{ config('app.name') }} Logo" width="160"></a>
-            </div>
-
-            <div class="col-6 col-lg-3 text-right order-lg-last">
-                {{--<div class="social">
-                    <a class="social-facebook" href="https://www.facebook.com/"><i class="fa fa-facebook"></i></a>
-                    <a class="social-twitter" href="https://twitter.com/"><i class="fa fa-twitter"></i></a>
-                    <a class="social-instagram" href="https://www.instagram.com//"><i class="fa fa-instagram"></i></a>
-                    <a class="social-dribbble" href="https://dribbble.com/"><i class="fa fa-dribbble"></i></a>
-                </div>--}}
-            </div>
-
-            <div class="col-lg-6">
-                <div class="nav nav-bold nav-uppercase nav-trim justify-content-lg-center">
+                <div class="nav nav-bold nav-uppercase nav-center">
                     <a class="nav-link" href="{{ url('/') }}">@lang('pages.menu.Home')</a>
                     <a class="nav-link" href="{{ url('/projects') }}">@lang('pages.menu.Projects')</a>
                     <a class="nav-link" href="{{ url('/contact') }}">@lang('pages.menu.Contact')</a>
                 </div>
-            </div>
 
+                <hr>
+
+                <p class="text-center">We’re a team of experienced designers and developers. We can combine beautiful, modern designs with clean, functional and high-performance code to produce stunning websites.</p>
+
+            </div>
         </div>
     </div>
 </footer><!-- /.footer -->
