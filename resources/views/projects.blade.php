3e661a5bd976140c249d9f1e4b6c9394f49851ea
Etem <etem@fornaxstudio.com>
2018-08-04 16:14:52 +0300

    mini header placement fix for projects page. text added to projects page

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -2,22 +2,22 @@
 @section('title', __('pages.title.Projects'))
 @section('content')
     <!-- Header -->
-    <header class="header pb-9 pt-10 mb-9" id="project-header">
-        <div class="container">
+    <header class="header pb-7 pt-4 mt-0" id="project-header">
+        {{--<div class="container">
             <div class="row">
                 <div class="col-md-6">
                     <h1 class="fw-200 mb-6">@lang('pages.title.Projects')</h1>
                     <p class="lead-2">@lang('pages.page-line.Projects')</p>
                 </div>
             </div>
-        </div>
+        </div>--}}
     </header><!-- /.header -->
 
     <main class="main-content">
         <div class="container">
-            <header class="section-header">
+            <header class="section-header pt-6">
                 <small>@lang('pages.title.Projects')</small>
-                <h2>@lang('pages.projects.We Have More to Discover')</h2>
+                <h2>@lang('pages.projects.There is always something more to learn')</h2>
                 <hr>
                 <p class="lead">@lang('pages.projects.Lead Text')</p>
             </header>
@@ -25,12 +25,12 @@
 
             <div class="row gap-y align-items-center">
                 <div class="col-md-6 ml-auto">
-                    <h4>5 Ready Samples</h4>
-                    <p>Think the or organization same proposal to affected heard reclined in be it reassuring are attained opinion, by he neighbours even again. Is and by value and that like can could from world takes of great copy and delicacy compared he such that the in and with himself a the man, client totally least for him and have insidious than of him remedies. That his counter-productive clearly, wasn't day we hills parts he on every.</p>
+                    <h4>@lang('pages.projects.Project1')</h4>
+                    <p>@lang('pages.projects.Project1 Text')</p>
                 </div>
 
                 <div class="col-md-5 order-md-first">
-                    <img class="rounded shadow-2" src="http://via.placeholder.com/600x400" alt="...">
+                    <img class="rounded shadow-2" src="{{ asset('assets/img/preview/dngo.png') }}" alt="...">
                 </div>
             </div>
 
@@ -40,12 +40,12 @@
 
             <div class="row gap-y align-items-center">
                 <div class="col-md-6 mr-auto">
-                    <h4>6 Header Types</h4>
-                    <p>Think the or organization same proposal to affected heard reclined in be it reassuring are attained opinion, by he neighbours even again. Is and by value and that like can could from world takes of great copy and delicacy compared he such that the in and with himself a the man, client totally least for him and have insidious than of him remedies. That his counter-productive clearly, wasn't day we hills parts he on every.</p>
+                    <h4>@lang('pages.projects.Project2')</h4>
+                    <p>@lang('pages.projects.Project2 Text')</p>
                 </div>
 
                 <div class="col-md-5">
-                    <img class="rounded shadow-2" src="http://via.placeholder.com/600x400" alt="...">
+                    <img class="rounded shadow-2" src="{{ asset('assets/img/preview/rezervapp.png')}}" alt="...">
                 </div>
             </div>
 
@@ -53,16 +53,16 @@
             <hr class="my-8">
 
 
-            <div class="row gap-y align-items-center">
+            {{--<div class="row gap-y align-items-center">
                 <div class="col-md-6 ml-auto">
-                    <h4>12 Common Pages</h4>
-                    <p>Think the or organization same proposal to affected heard reclined in be it reassuring are attained opinion, by he neighbours even again. Is and by value and that like can could from world takes of great copy and delicacy compared he such that the in and with himself a the man, client totally least for him and have insidious than of him remedies. That his counter-productive clearly, wasn't day we hills parts he on every.</p>
+                    <h4>@lang('pages.projects.Project3')</h4>
+                    <p>@lang('pages.projects.Project3 Text')</p>
                 </div>
 
                 <div class="col-md-5 order-md-first">
                     <img class="rounded shadow-2" src="http://via.placeholder.com/600x400" alt="...">
                 </div>
-            </div>
+            </div>--}}
 
 
         </div>
