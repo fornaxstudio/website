cdbc0cd9a63b390783eb6698909a812e6813403b
frnxstd <burak@myself.com>
2018-10-04 15:10:24 +0300

    HubSpot tracking code

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -29,7 +29,7 @@
 @yield('page')
 <script src="{{ asset('assets/js/compiled.js') }}"></script>
 {!! NoCaptcha::renderJs() !!}
-<!-- Global site tag (gtag.js) - Google Analytics -->
+<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4753846.js"></script>
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-123394344-1"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
