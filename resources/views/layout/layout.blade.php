ea6b3753179c0319d60cfefd287343d71496cb6b
ikidnapmyself <burakalbayrak@live.com>
2019-03-08 14:44:46 +0300

    hotjar tracking code fixed #FB-99

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -40,13 +40,13 @@
 </script>
 <script>
     (function(h,o,t,j,a,r){
-        h.hj=h.hjfunction(){(h.hj.q=h.hj.q[]).push(arguments)};
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:1227186,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
-    })(window,document,'https://static.hotjar.com/c/hotjar-%27,%27.js?sv=%27);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
 </script>
 @stack('scripts')
 <script>@stack('code')</script>
